828f95adc6d0372b009c835321b81dc2af445fe3
Darknab <117382045+Darknab@users.noreply.github.com>
2024-04-15 20:21:50 +0100

    Write BackButton and EditButton components

---
+++ src/Buttons.jsx
@@ -0,0 +1,9 @@
+export function BackButton({ onClick }) {
+  return <button type="button" onClick={onClick} >Back</button>
+}
+
+
+export function EditButton({ onClick }) {
+  return <button type="button" onClick={onClick} >Edit</button>
+}
+
