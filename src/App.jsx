91575cb915fe0b2af31b67a8a90cba694190f53f
Darknab <117382045+Darknab@users.noreply.github.com>
2024-04-16 18:37:16 +0100

    Add Education to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Education from './Education'
 import Header from './Header'
 import Summary from './Summary'
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <Summary />
+      <Education />
     </>
   )
 }
