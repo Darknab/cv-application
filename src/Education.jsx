bc04141d1a1147bf351b1c00b0eba9f6404d90ca
Darknab <117382045+Darknab@users.noreply.github.com>
2024-04-16 19:26:31 +0100

    Write the add functionality

--- src/Education.jsx
+++ src/Education.jsx
@@ -1,35 +1,46 @@
 import Form from './Form';
+import { AddButton } from './Buttons';
+import { useState } from 'react';
+
+const educationFields = [
+  {
+    name: 'educationTitle',
+    type: 'text',
+    value: '[Name of the school or university]',
+  },
+  {
+    name: 'educationDescription',
+    type: 'text',
+    value: '[Degree or description of the curriculum]',
+  },
+  {
+    name: 'yearStart',
+    type: 'select',
+    value: '[start]',
+  },
+  {
+    name: 'yearEnd',
+    type: 'select',
+    value: '[end]',
+  }
+]
 
 export default function Education() {
+  const [ forms, setForms ] = useState([<Form key={0} formName="education" fields={educationFields}/>])
+
+  function addForm() {
+    setForms([
+      ...forms,
+      <Form key={forms.length - 1} formName="education" fields={educationFields}/>
+    ])
+  }
   return (
     <div>
       <h2>Education</h2>
-      <Form 
-        formName="education"
-        fields={[
-          {
-            name: 'educationTitle',
-            type: 'text',
-            value: '[Name of the school or university]',
-          },
-          {
-            name: 'educationDescription',
-            type: 'text',
-            value: '[Degree or description of the curriculum]',
-          },
-          {
-            name: 'yearStart',
-            type: 'select',
-            value: '[start]',
-          },
-          {
-            name: 'yearEnd',
-            type: 'select',
-            value: '[end]',
-          }
-        ]}
-      
-      />
+      {forms.map((form, index) => {
+        return <div key={index}> {form} </div>
+      })}
+      <AddButton onClick={addForm} />
     </div>
   )
 }
